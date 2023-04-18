9549682a7f97f1d4c432ee53a0afed55dd8043c0
KaedenOC <komeacarr2@gmail.com>
2023-04-18 15:21:54 -0700

    env sample and api endpoint /weather

--- server.js
+++ server.js
@@ -3,3 +3,52 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+let weatherData = require('./data/weather.json');
+// console.log(weatherData);
+
+
+// app === server - need to call Express to create server
+const app = express();
+
+// MIDDLEWARE - allow anyone to hit our server
+
+app.use(cors());
+
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => console.log(`yay connecting to server ${PORT}`));
+
+// ENDPOINTS
+// 1st arg - endpoint url as a string
+//2nd arg - callback - will execute when that endpoint is hit
+// 2 params, request, response
+
+app.get('/weather', (request, response) => {
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+  const searchQuery = request.query.searchQuery;
+});
+
+// app.get('/', (request, response) => {
+//   response.status(200).send('welcome to server');
+// });
+
+// app.get('/hello', (request, response) => {
+//   let firstName = request.query.firstName;
+//   let lastName = request.query.lastName;
+
+//   response.status(200).send(`Hello ${firstName} ${lastName}`);
+// });
+
+//CATCHALL endpoint should be last defined
+
+app.get('*', (request, response) => {
+  response.status(404).send('page does not exist');
+});
+
+
+app.use((error, request, response, next) => {
+  console.log(error.message);
+  response.status(500).send(error.message);
+});
